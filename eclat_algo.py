131512f4e263cda78abed2067ac07faddc27c733
Trịnh Ngọc Các <cactrinh5@gmail.com>
2024-12-03 14:31:39 +0700

    refactor eclat
    refactor eclat

--- eclat_algo.py
+++ eclat_algo.py
@@ -1,16 +1,39 @@
 from math import ceil
+from copy import deepcopy
 
 from pandas import DataFrame
 
+
+def _vertical_transform(df: DataFrame, item_col: str) -> tuple[DataFrame, str]:
+    vert_df = df.groupby(item_col)\
+                .aggregate(set)
+    transact_col = vert_df.columns[0]
+
+    vert_df = vert_df.sort_values(transact_col, axis=0,
+                                  key=lambda series: series.apply(len))
+    return vert_df, transact_col
+
+def _find_common_prefix(itemset1: list, itemset2: list) -> list:
+    if len(itemset1) != len(itemset2):
+        raise ValueError("itemsets have different length!")
+    
+    for i, item in enumerate(itemset1):
+        if item != itemset2[i]: break
+    
+    if i - 1 < 0: return []
+    else: return deepcopy(itemset1[:i])
+
 def eclat(df: DataFrame, min_support: float, item_col: str, *, max_iter: int = 100_000):
-    vert_df, transact_col = vertical_transform(df, item_col)
+    vert_df, transact_col = _vertical_transform(df, item_col)
     minFreq = ceil(min_support * len(df[transact_col].unique()))
+
+    vert_df = vert_df[vert_df[transact_col].apply(len) >= minFreq]
     
     equiv_classes = [[]]
     freq_itemset = []
-    for item, transacts in zip(vert_df.index, vert_df[transact_col]):
-        equiv_classes[0].append(([item], transacts))
-        freq_itemset.append(([item], len(transacts)))
+    for item, transacts in vert_df.iterrows():
+        equiv_classes[0].append(([item], transacts.iloc[0]))
+        freq_itemset.append(([item], len(transacts.iloc[0])))
 
     while equiv_classes and (max_iter := max_iter - 1):
         cur_class = equiv_classes.pop()
@@ -21,7 +44,7 @@ def eclat(df: DataFrame, min_support: float, item_col: str, *, max_iter: int = 1
                 new_transacts = transacts & transacts2
                 if len(new_transacts) < minFreq: continue
                 
-                new_itemset = find_common_prefix(itemset, itemset2)
+                new_itemset = _find_common_prefix(itemset, itemset2)
                 new_itemset.append(itemset[-1])
                 new_itemset.append(itemset2[-1])
                 new_itemset = sorted(new_itemset, key=lambda item: len(vert_df.loc[item][transact_col]))
@@ -31,22 +54,3 @@ def eclat(df: DataFrame, min_support: float, item_col: str, *, max_iter: int = 1
             
             if next_class: equiv_classes.append(next_class)
     return freq_itemset
-
-def vertical_transform(df: DataFrame, item_col: str) -> tuple[DataFrame, str]:
-    vert_df = df.groupby(item_col)\
-                .aggregate(set)
-    transact_col = vert_df.columns[0]
-
-    vert_df = vert_df.sort_values(transact_col, axis=0,
-                                  key=lambda series: series.apply(len))
-    return vert_df, transact_col
-
-def find_common_prefix(itemset1: list, itemset2: list) -> list:
-    if len(itemset1) != len(itemset2):
-        raise ValueError("itemsets have different length!")
-    
-    for i, item in enumerate(itemset1):
-        if item != itemset2[i]: break
-    
-    if i - 1 < 0: return []
-    else: return itemset1[:i]
